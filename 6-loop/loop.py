e02366aa815bd1c426e8ab98b5f0cc289e091546
quantaolin <linqt2006@163.com>
2017-12-24 21:57:22 +0800

    1

--- 6-loop/loop.py
+++ 6-loop/loop.py
@@ -3,3 +3,15 @@ Created on 2017年12月23日
 
 @author: linqt
 '''
+flag = 0
+while flag < 5:
+    print(flag)
+    flag+=1
+else:
+    print("flag bigger than five")
+    
+str="hello world!"
+for tmp in str:
+    print(tmp)
+else:
+    print("this is end")
