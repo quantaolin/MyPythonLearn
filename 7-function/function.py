09fe00fc8998f34c82198f66798a386f77dff080
linqt <linqt@windows10.microdone.cn>
2017-12-25 22:45:50 +0800

    1

--- 7-function/function.py
+++ 7-function/function.py
@@ -3,7 +3,10 @@ Created on 2017年12月23日
 
 @author: linqt
 '''
+def fun1(arg1,arg2):
+    arg3 = arg1+arg2
+    return arg3
+print(fun1(2,3))
 
-#函数式编程，很好很强大
-fun1 = lambda arg1,arg2: arg1+arg2
-print(fun1(1,2))
+fun2 = lambda arg1,arg2: arg1+arg2
+print(fun2(1,2))
