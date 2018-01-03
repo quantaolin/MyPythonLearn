7983000ee86db2ebb43b0c816e3ff940d255bc3c
linqt <linqt@windows10.microdone.cn>
2018-01-03 22:44:38 +0800

    1

--- k-multithreading/multithreading.py
+++ k-multithreading/multithreading.py
@@ -3,3 +3,15 @@ Created on 2017年12月27日
 
 @author: linqt
 '''
+import time
+import threading
+
+class myThread(threading.Thread):
+    
+    def __init__(self,name,sleeptime):
+        self.name = name
+        self.sleeptime = sleeptime
+    def run(self):
+        for tmp in range[0,5]:
+            print(self.name + ":" + tmp)
+            time.sleep(self.sleeptime)
