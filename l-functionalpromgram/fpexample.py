7e9af1b89a034d282ea6f9d61bddebab0478790e
quantaolin <linqt2006@163.com>
2018-01-07 22:25:37 +0800

    1

--- l-functionalpromgram/fpexample.py
+++ l-functionalpromgram/fpexample.py
@@ -3,3 +3,10 @@ Created on 2018年1月7日
 
 @author: linqt
 '''
+from functools import reduce
+from decimal import *
+getcontext().prec = 8
+def getSum(amountList):
+    return reduce(lambda x,y:x+y,map(lambda x:(Decimal(str(x))%Decimal("1")*Decimal("100"))%Decimal("1")/Decimal("100"),amountList))
+
+print(getSum([5.123,2.123,12.1551231,15.2123]))
